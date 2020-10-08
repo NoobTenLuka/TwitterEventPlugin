322e046cdde6b4f269499c6ae9d3ded28b2a7718
NoobTenLuka <NoobTenLuka@gmail.com>
2020-10-08 18:27:23 +0200

    fix an issue with the scope of the variables defined in the example.start.sh

--- example.start.sh
+++ example.start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-twitter4j.debug=true
-twitter4j.oauth.consumerKey=*************************
-twitter4j.oauth.consumerSecret=**************************************************
-twitter4j.oauth.accessToken=*******************-******************************
-twitter4j.oauth.accessTokenSecret=*********************************************
+export twitter4j.debug=true
+export twitter4j.oauth.consumerKey=*************************
+export twitter4j.oauth.consumerSecret=**************************************************
+export twitter4j.oauth.accessToken=*******************-******************************
+export twitter4j.oauth.accessTokenSecret=*********************************************
 
 java -Xms#G -Xmx#G -XX:+UseG1GC -jar spigot.jar nogui
